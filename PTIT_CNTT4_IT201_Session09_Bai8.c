9e3ef22efe4a6dadd161f2d69391c113395bb4b2
khanhh89 <khanhabcabc0000@gmail.com>
2025-07-02 22:50:43 +0700

    SS9

--- PTIT_CNTT4_IT201_Session09_Bai8.c
+++ PTIT_CNTT4_IT201_Session09_Bai8.c
@@ -44,7 +44,6 @@ Node* removeHead(Node* head) {
     if (head == NULL) return NULL;
     Node* temp = head;
     head = head->next;
-    free(temp);
     return head;
 }
 Node* removeLast(Node* head) {
